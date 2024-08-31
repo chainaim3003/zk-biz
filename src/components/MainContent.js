f6cef04a23f5467697019608143be9a6c1e085d9
chainaim3003 <krisat3003@gmail.com>
2024-08-31 13:58:37 -0400

    caps again

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,7 +4,7 @@ function MainContent() {
   return (
     <main className="main-content">
       <h1>ZK Business Process Engine</h1>
-      <p> Powering the Next generation Finance for Supplychain, Insurance and Ppayments</p>
+      <p> Powering the Next generation Finance for Supplychain, Insurance and Payments</p>
       <div className="cta-buttons">
         <a href="#" className="cta-button">Click</a>
         <br></br>
