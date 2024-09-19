eead30571122016e7ea094ccd32e07803af011cd
chainaim3003 <krisat3003@gmail.com>
2024-09-19 14:21:14 -0400

    main content changes

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,7 +6,7 @@ function MainContent() {
       <h1>ZK Business Process Engine</h1>
       <p> Powering the Next generation Finance for Supplychain, Insurance and Payments</p>
       <div className="cta-buttons">
-        <a href="#" className="cta-button">Click</a>
+        <a href="#" className="cta-button"></a>
         <br></br>
         <br></br>
         
